922550aa14940ed56477826023e9b9c8f0131f51
luoyin <luoyin1994@icloud.com>
2017-01-16 22:50:50 +0800

    deleted testdata

--- testdata/pages/index/movies.js
+++
@@ -1,9 +0,0 @@
-var movies = []
-for (var i = 0; i < 6; i++) {
-    movies.push({
-        title : "机械战警",
-        _id   : i + 1,
-        poster: "http://i3.shouyou.itc.cn/v3/news/2014/01/03/0103155012197137479.jpg"
-    })
-}
-module.exports = movies
